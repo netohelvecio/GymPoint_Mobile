faec8e2b3b4a7b7727cc9d6d168cb8c7e5be36b9
netohelvecio <helvecioneto77@gmail.com>
2019-12-29 01:13:29 -0300

    autenticação no app

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useState } from 'react';
+import { Image, ActivityIndicator } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.png';
 import { Container, Input, SubmitButton, SubmitButtonText } from './styles';
 
 export default function SignIn() {
+  const [id, setId] = useState('');
+  const loading = useSelector(state => state.auth.loading);
+
+  const dispatch = useDispatch();
+
+  function handleSubmit() {
+    dispatch(signInRequest(id));
+  }
+
   return (
     <Container>
       <Image source={logo} />
@@ -12,12 +24,18 @@ export default function SignIn() {
       <Input
         placeholder="Informe seu ID de cadastro"
         returnKeyType="send"
-        onSubmitEditing={() => {}}
+        onSubmitEditing={handleSubmit}
         keyboardType="numeric"
+        value={id}
+        onChangeText={setId}
       />
 
-      <SubmitButton onPress={() => {}}>
-        <SubmitButtonText>Entrar no sistema</SubmitButtonText>
+      <SubmitButton onPress={handleSubmit}>
+        {loading ? (
+          <ActivityIndicator size={20} color="#fff" />
+        ) : (
+          <SubmitButtonText>Entrar no sistema</SubmitButtonText>
+        )}
       </SubmitButton>
     </Container>
   );
